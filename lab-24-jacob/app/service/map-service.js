76d5ded38feb77da9c39749b63f081354b445496
jisenber <isenberg.jacob@yahoo.com>
2017-02-12 11:37:48 -0800

    scaffolding done, dependencies added, services written

---
+++ lab-24-jacob/app/service/map-service.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const angular = require('angular');
+const scottAdventure = angular.module('Scott-Adventure');
+
+scottAdventure.factory('mapService', ['$log', mapService]);
+
+function mapService($log) {
+  $log.debug('map service');
+
+  let service = {}; //want to attach methods to this
+
+  //Build map data object
+  service.mapData = {
+    cabin: {
+      desc: 'message from the cabin',
+      south: 'trail'
+    },
+    trail: {
+      desc: 'message from the trail',
+      north: 'cabin',
+      east: 'gate',
+      south: 'pit'
+    },
+    pit: {
+      desc: 'message from the pit',
+      north: 'trail'
+    },
+    gate: {
+      desc: 'message from the gate',
+      west: 'trail',
+      east: 'castle'
+    },
+    castle: {
+      desc: 'message from the castle',
+      west: 'gate',
+      south: 'corridor'
+    },
+    corridor: {
+      desc: 'message from the corridor',
+      north: 'castle',
+      east: 'snackroom'
+    },
+    snackroom: {
+      desc: 'message from the snack room',
+      west: 'corridor'
+    }
+  };
+}
---
+++ lab-24-jacob/app/service/player-service.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const angular = require('angular');
+const scottAdventure = angular.module('Scott-Adventure');
+
+scottAdventure.factory('playerService', ['$q', '$log', 'mapService', playerService]);
+
+function playerService($q, $log, mapService) {
+  $log.debug('player service');
+
+  let service = {};
+
+  let turn = 0;
+  let player = service.player = {
+    name: 'scott',
+    location: 'cabin',
+    hp: 16
+  };
+
+  let history = service.history = [
+    {
+      turn,
+      desc: 'Welcome to Scott\'s wacky and crazy adventure!',
+      location: 'cabin',
+      hp: player.hp
+    }
+  ];
+
+  service.movePlayer = function(direction) {
+    return new $q((resolve, reject) => {
+      turn ++;
+
+      let current = player.location;  //starting point
+      let newLocation = mapService.mapData[current][direction];
+      //user inputs 'direction' when playing game
+
+      if(!newLocation) {
+        history.unshift({  //adds this object to beginning of history array
+          turn,
+          desc: 'You have run into a wall',
+          location: player.location,
+          hp: player.hp
+        });
+        return reject('no room in that direction');
+      }
+      history.unshift ({
+        turn,location: player.location,
+        desc: mapService.mapData[newLocation].desc,
+        hp: player.hp
+      });
+      //Reassign the player's location
+      player.location = newLocation;
+      return resolve(player.location);
+    });
+  };
+  return service;
+}
