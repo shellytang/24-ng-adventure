9f0c4047f33374544c05a86f11cb1e3e31b5646c
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-02-12 11:40:28 -0800

    Setting up files....sooo many files

---
+++ lab-jessica/webpack.config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/entry.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/build',
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: './app/index.html',
+    }),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+};
