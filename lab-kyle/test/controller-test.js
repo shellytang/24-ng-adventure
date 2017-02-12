0f23a9e128759b6de0977ef0834e899845cb0e65
Kyle Winckler <kyle.winckler@gmail.com>
2017-02-12 15:55:45 -0800

    fix

--- lab-kyle/test/controller-test.js
+++ lab-kyle/test/controller-test.js
@@ -16,21 +16,23 @@ describe('App Controllers', function() {
       })
     })
 
-    it('has a history property that is an array with one item', () => {
-      expect(Array.isArray(this.historyCtrl.history)).toBe(true)
-      expect(this.historyCtrl.history.length).toBe(1)
-    })
+    describe('inital properties', () => {
+      it('has a history property that is an array with one item', () => {
+        expect(Array.isArray(this.historyCtrl.history)).toBe(true)
+        expect(this.historyCtrl.history.length).toBe(1)
+      })
 
-    it('history array item at index 0 has turn property set to 0', () => {
-      expect(this.historyCtrl.history[0].turn).toBe(0)
-    })
+      it('history array item at index 0 has turn property set to 0', () => {
+        expect(this.historyCtrl.history[0].turn).toBe(0)
+      })
 
-    it('history[0] has desc property equal to Welcome to your adventure! Good Luck!', () => {
-      expect(this.historyCtrl.history[0].desc).toBe('Welcome to your adventure! Good luck!')
-    })
+      it('history[0] has desc property equal to Welcome to your adventure! Good Luck!', () => {
+        expect(this.historyCtrl.history[0].desc).toBe('Welcome to your adventure! Good luck!')
+      })
 
-    it('history[0] has a location property set to home', () => {
-      expect(this.historyCtrl.history[0].location).toBe('home')
+      it('history[0] has a location property set to home', () => {
+        expect(this.historyCtrl.history[0].location).toBe('home')
+      })
     })
   })
 
