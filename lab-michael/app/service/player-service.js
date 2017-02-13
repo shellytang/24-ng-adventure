db572ff8b9d52325a597ac45e0bfab8ec03ccf0f
Michael Bishop <polishthebishop@gmail.com>
2017-02-12 23:37:27 -0800

    services done

---
+++ lab-michael/app/service/player-service.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const angular = require('angular');
+const ngAdventure = angular.module('ngAdventure');
+
+ngAdventure.factory('playerService', ['$q', '$log', 'mapService', playerService]);
+
+function playerService($q, $log, mapService) {
+  $log.debug('player service');
+
+  let service = {};
+
+  let turn = 0;
+  let player = service.player = {
+    name: 'Hans Steinbeck',
+    location: 'Willkomen zum Oktoberfest(Eingang)',
+    hp: 20
+  };
+
+  let history = service.history = [
+    {
+      turn,
+      desc: 'Welcome to Die Wies\'n',
+      location: 'Eingang(Entrance)',
+      hp: player.hp
+    }
+  ];
+
+  service.movePlayer = function(direction) {
+    return new $q((resolve, reject) => {
+      turn++;
+
+      let current = player.location;
+      let newLocation = mapService.mapData[current][direction];
+
+      if (!newLocation) {
+        history.unshift({
+          turn,
+          desc: 'You have run into a wall',
+          location: player.location,
+          hp: player.hp
+        });
+        return reject('Die Polizei hier!');
+      };
+
+      history.unshift({
+        turn,
+        location: player.location,
+        desc: mapService.mapData[newLocation].desc,
+        hp: player.hp
+      });
+
+      player.location = newLocation;
+      return resolve(player.location);
+    });
+  };
+
+  return service;
+};
