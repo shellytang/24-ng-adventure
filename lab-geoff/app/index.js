259fad56cf70be201f455b92901dbf24658d154b
Geoffrey Cox <gcox3d@gmail.com>
2017-02-13 00:23:36 -0800

    added require for services and components

--- lab-geoff/app/index.js
+++ lab-geoff/app/index.js
@@ -4,5 +4,10 @@ let angular = require('angular');
 angular.module('mazeApp', []);
 
 //require services
+require('./service/playerService.js');
+require('./service/mapService.js');
 
 //require components
+require('./component/gamePad/gamePad.js');
+require('./component/history/history.js');
+require('./component/player/player.js');
