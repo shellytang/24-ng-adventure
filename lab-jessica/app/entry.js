0e07f7a4201f0775b5c98063e7d15c9302d455e9
Jessica Soltero <jmichellevs@gmail.com>
2017-02-14 14:20:39 -0800

    finished player info

--- lab-jessica/app/entry.js
+++ lab-jessica/app/entry.js
@@ -8,4 +8,5 @@ require('./service/map-service.js');
 require('./service/player-service.js');
 
 require('./component/game-pad/game-pad.js');
+require('./component/player-info/player-info.js');
 require('./component/game-history/game-history.js');
