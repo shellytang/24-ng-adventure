372c91a81772c0dd5f6fc6b064d06b0d56aed760
Shelly Tang <shellytang@gmail.com>
2017-05-26 12:11:58 -0700

    format tags and test setup

--- lab-shelly/test/gamepad-controller-test.js
+++ lab-shelly/test/gamepad-controller-test.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const angular = require('angular');
+require('angular-mocks');
+
+describe('testing the game-pad controller', function() {
+
+  beforeEach(() => {
+    angular.mock.module('ngAdventure');
+    angular.mock.inject(($rootScope, $controller) => {
+      this.$rootScope = $rootScope;
+      this.gamepadCtrl = new $controller('gamepadCtrl');
+      this.gamepadCtrl.$onInit();
+    });
+  });
+
+  afterEach(() => this.$rootScope.$apply());
+
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+
+});
